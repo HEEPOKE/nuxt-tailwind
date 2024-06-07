8f423259b23989c7accbd93bc4c6157e85b3e28d
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2024-06-07 11:41:49 +0700

    feat: add module :money_with_wings:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,24 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/ui"],
-
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@nuxtjs/google-fonts",
+    "@nuxt/eslint",
+    "nuxt-security",
+    "dayjs-nuxt",
+    "nuxt-booster",
+  ],
+  eslint: {
+    config: {
+      stylistic: {
+        indent: "tab",
+        semi: true,
+      },
+    },
+  },
   tailwindcss: {
     cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
     configPath: "tailwind.config",
@@ -9,5 +26,54 @@ export default defineNuxtConfig({
     viewer: true,
     config: {},
     editorSupport: true,
+  },
+  googleFonts: {
+    families: {
+      Roboto: true,
+      "Josefin+Sans": true,
+      Lato: [100, 300],
+      Raleway: {
+        wght: [100, 400],
+        ital: [100],
+      },
+      Inter: "200..700",
+      "Crimson Pro": {
+        wght: "200..900",
+        ital: "200..700",
+      },
+    },
+  },
+  security: {
+    headers: {},
+    rateLimiter: {},
+  },
+  dayjs: {
+    defaultLocale: [
+      "th",
+      {
+        relativeTime: {
+          future: "in %s",
+          past: "%s ago",
+          s: "a few secondses",
+          m: "a minute",
+          mm: "%d minuteses",
+          h: "an hour",
+          hh: "%d hourses",
+          d: "a day",
+          dd: "%d dayses",
+          M: "a month",
+          MM: "%d monthseses",
+          y: "a year",
+          yy: "%d yearseses",
+        },
+      },
+    ],
+  },
+  booster: {
+    detection: {
+      performance: true,
+      browserSupport: true,
+      battery: true,
+    },
   },
 });
