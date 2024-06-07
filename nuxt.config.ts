a60de05becdf05b297904018b49ae0773733e468
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2024-06-07 11:19:00 +0700

    feat: add nuxt/ui && tailwind :money_with_wings:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/ui"],
+
+  tailwindcss: {
+    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
+    configPath: "tailwind.config",
+    exposeConfig: true,
+    viewer: true,
+    config: {},
+    editorSupport: true,
+  },
 });
